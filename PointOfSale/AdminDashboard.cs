06c7dc4f08518e65fe3d7ca63d26088dabb09308
Aadi Poddar <aadipoddarmail@gmail.com>
2023-04-17 18:07:30 +0530

    Added Database Connection

--- PointOfSale/AdminDashboard.cs
+++ PointOfSale/AdminDashboard.cs
@@ -1,9 +1,19 @@
-﻿namespace PointOfSale;
+﻿using Microsoft.Data.SqlClient;
+
+namespace PointOfSale;
 
 public partial class AdminDashboard : Form
 {
+    SqlConnection cn = new SqlConnection();
+    SqlCommand cm = new SqlCommand();
+    DBConnection dbcon = new DBConnection();
+
     public AdminDashboard()
     {
         InitializeComponent();
+        cn = new SqlConnection(dbcon.MyConnection());
+        cn.Open();
+        MessageBox.Show("Connected");
+        cn.Close();
     }
 }
---
+++ PointOfSale/DBConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace PointOfSale;
+
+class DBConnection
+{
+    public string MyConnection()
+    {
+        string con = "Data Source=AADILAPI\\SQLEXPRESS;Initial Catalog=POS_DEMO_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        return con;
+    }
+}
