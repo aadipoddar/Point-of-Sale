f9619598a57f5bb8fa47f1e3ee904785f07f06b0
Aadi Poddar <aadipoddarmail@gmail.com>
2023-05-03 02:20:38 +0530

    Removed Backup Files

--- WinForms/PointOfSaleWinFormsLibrary.backup/Models/BrandModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WinForms.PointOfSaleLibrary.Models
-{
-    public class BrandModel
-    {
-        public int Id { get; set; }
-        public string Brand { get; set; }
-    }
-}
--- WinForms/PointOfSaleWinFormsLibrary.backup/Models/CategoryModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WinForms.PointOfSaleLibrary.Models
-{
-    public class CategoryModel
-    {
-        public int Id { get; set; }
-        public string Category { get; set; }
-    }
-}
